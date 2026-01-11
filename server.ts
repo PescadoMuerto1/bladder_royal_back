6affd7f5660fb01c58599753970a9b503bb6fe2f
PescadoMuerto1 <mbl0502727793@gmail.com>
2026-01-11 15:46:06 +0200

    feat: add basic Express server setup
    - Configure Express with CORS and cookie parser
    - Set up development and production environments
    - Add basic route structure

---
+++ server.ts
@@ -0,0 +1,50 @@
+import 'dotenv/config'
+import 'dotenv/config'
+import http from 'http'
+import path from 'path'
+import cors from 'cors'
+import express, { Express } from 'express'
+import cookieParser from 'cookie-parser'
+
+import { authRoutes } from './api/auth/auth.routes.js'
+import { userRoutes } from './api/user/user.routes.js'
+import { setupSocketAPI } from './services/socket.service.js'
+import { logger } from './services/logger.service.js'
+
+const app: Express = express()
+const server = http.createServer(app)
+
+// Express App Config
+app.use(cookieParser())
+app.use(express.json())
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve('public')))
+  console.log('public')
+} else {
+  const corsOptions = {
+    origin: [
+      'http://127.0.0.1:3000',
+      'http://localhost:3000',
+      'http://127.0.0.1:5173',
+      'http://localhost:5173'
+    ],
+    credentials: true
+  }
+  console.log(corsOptions)
+  app.use(cors(corsOptions))
+}
+
+// routes
+app.use('/api/auth', authRoutes)
+app.use('/api/user', userRoutes)
+setupSocketAPI(server)
+
+app.get('/**', (_req, res) => {
+  res.sendFile(path.resolve('public/index.html'))
+})
+
+const port = process.env.PORT || 3030
+server.listen(port, () => {
+  logger.info('Server is running on port: ' + port)
+})
